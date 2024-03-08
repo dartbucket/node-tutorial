c46c3dd8f4e27d28c234c8bc94a2320c54f7ab47
“DartBucket” <dartbuckets@gmail.com>
2024-03-09 03:09:15 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
-const server = http.createServer((request, response)=>{
+const server = http.createServer((req, res)=>{
+
+    console.log(req.url);
+
+    console.log(req.method);
+
+    // GET, POST, PUT , DELETE
     console.log("Request Hit");
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write("Hello World");
+
+    res.end();
 });
 
 server.listen(3000, () => {
